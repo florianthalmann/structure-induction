4e6c79773d06b5606b72f86c9319f182ac49c8a7
Florian Thalmann <florianthalmann@bluewin.ch>
2017-03-25 14:23:54 +0000

    updated exports and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { StructureInducer, IterativeSmithWatermanResult } from './structure'
-export { Similarity } from './similarity'
-export { Quantizer } from './quantizer'
-export { SmithWaterman } from './smith-waterman'
+export { StructureInducer, IterativeSmithWatermanResult } from './structure';
+export { Similarity } from './similarity';
+export { Quantizer, QUANT_FUNCS } from './quantizer';
+export { SmithWaterman } from './smith-waterman';
+export { HEURISTICS } from './heuristics';
+export { OPTIMIZATION } from './siatec';
